035b4a5985d3414f577f8930f73539cb441bc2a2
stuartmcphersonflipmind <stuart.mcpherson@flipmind.com>
2021-07-09 10:07:19 +1200

    history mode turned on

--- envparser.js
+++ envparser.js
@@ -6,7 +6,7 @@ module.exports = function (ctx) {
   parsedEnv['AIRCLAIM_CONFIGURATION_CONTRACT'] = ctx.dev ? "freeoscfga" : "freeoscfg4",
   parsedEnv['FREEOSTOKENS_CONTRACT'] = ctx.dev ? "freeostokena": "freeostoken4"
   parsedEnv['TIMED_FETCH_DELAY'] = ctx.dev ? 10000000 : 10000
-  parsedEnv['VUE_ROUTER_MODE'] = ctx.dev ? "history" : null
+  parsedEnv['VUE_ROUTER_MODE'] = ctx.dev ? null : "history"
 
   return parsedEnv
 }
