1f5ea1158ff1944081b161957a5493929cdfac66
stuartmcphersonflipmind <stuart.mcpherson@flipmind.com>
2021-08-03 14:27:57 +1200

    QA5 for prod v0.9.3

--- envparser.js
+++ envparser.js
@@ -2,9 +2,10 @@ const DotEnv = require('dotenv')
 const parsedEnv = DotEnv.config().parsed
 module.exports = function (ctx) {
 
-  parsedEnv['AIRCLAIM_CONTRACT'] = ctx.dev ? "freeos4" : "freeosa",
-  parsedEnv['AIRCLAIM_CONFIGURATION_CONTRACT'] = ctx.dev ? "freeoscfg4" : "freeoscfga",
-  parsedEnv['FREEOSTOKENS_CONTRACT'] = ctx.dev ? "freeostoken4": "freeostokena"
+  parsedEnv['AIRCLAIM_CONTRACT'] = ctx.dev ? "freeos4" : "freeos5",
+  parsedEnv['AIRCLAIM_CONFIGURATION_CONTRACT'] = ctx.dev ? "freeoscfg4" : "freeoscfg5",
+  parsedEnv['FREEOSTOKENS_CONTRACT'] = ctx.dev ? "freeostoken4": "freeostoken5"
+  parsedEnv['TOKEN_CURRENCY_NAME'] = ctx.dev ? "OPTION" : "POINT"
   parsedEnv['TIMED_FETCH_DELAY'] = ctx.dev ? 10000000 : 10000
   parsedEnv['VUE_ROUTER_MODE'] = ctx.dev ? null : "history"
 
