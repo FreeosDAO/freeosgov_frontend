f9dc534c18715c58887130dbed77c455d3e852e7
Stuart McPherson <stuart.mcpherson@flipmind.com>
2022-04-20 10:04:31 +1200

    v0.5.6 Vote button disabled on Airclaim Pending & Submit, Vote input tooltip remove final

--- src/pages/Vote.vue
+++ src/pages/Vote.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md">
     <CompleteDialog ref="complete" />
     <div class="q-gutter-y-md q-mx-auto" style="max-width: 600px">
-      <div class="panel-wrap">
+      <form class="panel-wrap" novalidate>
         <q-card class="panel q-pa-lg">
           <div class="text-h4 text-center q-ma-lg">Vote on Locking Threshold</div>
 
@@ -10,91 +10,69 @@
             <p class="q-mb-sm text-subtitle1" style="line-height:1.4;">
               <strong>Current Locking Threshold:</strong>
             </p>
-            <h4
-              class="text-h4 q-ma-xs"
-              style="line-height:1;"
-            >{{ targetPrice }} USD</h4>
+            <h4 class="text-h4 q-ma-xs" style="line-height:1;">{{ targetPrice }} USD</h4>
           </div>
 
 
-          <p class="text-subtitle1 q-mb-md" style="line-height:1.2"><strong>What price should the Locking Threshold be this week?</strong></p>
+          <p class="text-subtitle1 q-mb-md" style="line-height:1.2"><strong>What price should the Locking Threshold be
+              this week?</strong></p>
 
           <p class="q-mb-lg">
-              Increasing the Locking Threshold may help support the price of {{currencyName}}. However you may end up with more of your {{tokenCurrencyName}} Locked. For more information <router-link to="/info#vested-options">click here</router-link>
+            Increasing the Locking Threshold may help support the price of {{ currencyName }}. However you may end up with
+            more of your {{ tokenCurrencyName }} Locked. For more information <router-link to="/info#vested-options">click
+              here</router-link>
           </p>
 
 
-<section v-if="userHasVoted">
-                <div class="panel panel-info q-pa-lg text-center q-mb-lg q-pa-lg">
-                    <p class="q-mb-md text-h4" style="line-height:1.2;">You Voted!</p>
-                    <p class="q-mb-sm text-subtitle1" style="line-height:1.4;">Thanks for Voting this week</p>
-                </div>
+          <section v-if="userHasVoted">
+            <div class="panel panel-info q-pa-lg text-center q-mb-lg q-pa-lg">
+              <p class="q-mb-md text-h4" style="line-height:1.2;">You Voted!</p>
+              <p class="q-mb-sm text-subtitle1" style="line-height:1.4;">Thanks for Voting this week</p>
+            </div>
 
-</section>
+          </section>
 
-<section v-if="userHasStaked && !userHasVoted">
-          <div class="q-px-md q-py-sm">
-            <q-slider
-              v-model="lockingThresholdVote"
-              :min="thresholdRangeLower"
-              :max="thresholdRangeUpper"
-              :step="0.000001"
-              marker-labels
-              label
-              :label-always="!!lockingThresholdVote"
-              track-size="5px"
-              thumb-size="28px"
-            />
-          </div>
+          <section v-if="userHasStaked && !userHasVoted">
+            <div class="q-px-md q-py-sm">
+              <q-slider v-model="lockingThresholdVote" :min="thresholdRangeLower" :max="thresholdRangeUpper"
+                :step="0.000001" marker-labels label :label-always="!!lockingThresholdVote" track-size="5px"
+                thumb-size="28px" />
+            </div>
 
-          <div style="align-items: center;" class="row justify-center q-mb-sm q-pb-xs">
-            <div class="col-xs-8 col-sm-7 text-sm">Or manually enter amount:</div>
-            <div class="col-xs-1"></div>
-            <div class="col-xs-3 col-sm-4">
-              <label
-                data-v-052b630f
-                for="f_6eee53df-da8d-4f65-9fad-d55fd4c1e7e7"
-                class="q-field row no-wrap items-start q-input q-field--outlined q-field--dense q-validation-component"
-              >
-                <div class="q-field__inner relative-position col self-stretch">
-                  <div tabindex="-1" class="q-field__control relative-position row no-wrap">
-                    <div
-                      class="q-field__control-container col relative-position row no-wrap q-anchor--skip max-btn-holder"
-                    >
-                      <input
-                      novalidate
-                        style="text-align: center;"
-                        v-model="lockingThresholdVote"
-                        tabindex="0"
-                        id="f_6eee53df-da8d-4f65-9fad-d55fd4c1e7e7"
-                        type="number"
-                        class="q-field__native q-placeholder"
-                      />
+            <div style="align-items: center;" class="row justify-center q-mb-sm q-pb-xs">
+              <div class="col-xs-8 col-sm-7 text-sm">Or manually enter amount:</div>
+              <div class="col-xs-1"></div>
+              <div class="col-xs-3 col-sm-4">
+                <label data-v-052b630f for="f_6eee53df-da8d-4f65-9fad-d55fd4c1e7e7"
+                  class="q-field row no-wrap items-start q-input q-field--outlined q-field--dense q-validation-component">
+                  <div class="q-field__inner relative-position col self-stretch">
+                    <div tabindex="-1" class="q-field__control relative-position row no-wrap">
+                      <div
+                        class="q-field__control-container col relative-position row no-wrap q-anchor--skip max-btn-holder">
+                        <input novalidate style="text-align: center;" v-model="lockingThresholdVote" tabindex="0"
+                          id="f_6eee53df-da8d-4f65-9fad-d55fd4c1e7e7" type="number"
+                          class="q-field__native q-placeholder" />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </label>
+                </label>
+              </div>
             </div>
-          </div>
 
-          <p class="text-center text-negative q-mt-none q-mb-sm" v-if="lockingThresholdVoteInvalid && lockingThresholdVote !== 0">Locking Threashold must be between {{thresholdRangeLower}} and {{thresholdRangeUpper}}</p>
+            <p class="text-center text-negative q-mt-none q-mb-sm"
+              v-if="lockingThresholdVoteInvalid && lockingThresholdVote !== 0">Locking Threashold must be between
+              {{ thresholdRangeLower }} and {{ thresholdRangeUpper }}</p>
 
-          <div style="align-items: center;" class="row justify-center q-mt-md q-mb-sm q-pb-none">
-            <q-btn
-              size="lg"
-              unelevated
-              no-caps
-              outline
-              :disable="!userHasStaked || userHasVoted || !lockingThresholdVote"
-              color="primary"
-              @click="submit()"
-            >Submit Vote</q-btn>
-          </div>
+            <div style="align-items: center;" class="row justify-center q-mt-md q-mb-sm q-pb-none">
+              <q-btn size="lg" unelevated no-caps outline
+                :disable="!userHasStaked || userHasVoted || !lockingThresholdVote || airclaimStatus !== 'Running'" color="primary" @click="submit()">
+                Submit Vote</q-btn>
+            </div>
           </section>
         </q-card>
-        </div>
-      </div>
+      </form>
     </div>
+  </div>
 </template>
 
 
@@ -118,63 +96,62 @@ export default {
       thresholdRangeLower: parseFloat(process.env.VOTETHRESHOLDLOWER),
     }
   },
-      components: {
-        CompleteDialog
-    },
+  components: {
+    CompleteDialog
+  },
   computed: {
-    ...mapGetters('freeos', ['userHasVoted', 'lockFactor', 'userHasStaked', 'isAuthenticated', 'accountName', 'isRegistered', 'stakeRequirement', 'isFreeosEnabled', 'totalFreeos', 'liquidFreeos', 'liquidOptions', 'canClaim', 'reasonCannotClaim', 'currentIteration', 'nextIteration', 'airkeyBalance', 'airclaimStatus', 'currentPrice', 'targetPrice']),
-    thresholdRangeUpper(){
-        return this.currentPrice < this.thresholdRangeLower ? Math.floor(parseFloat(this.lockFactor) * this.thresholdRangeLower * 10000000) / 10000000 : Math.floor(parseFloat(this.lockFactor) * this.currentPrice * 10000000) / 10000000;
+    ...mapGetters('freeos', ['airclaimStatus', 'userHasVoted', 'lockFactor', 'userHasStaked', 'isAuthenticated', 'accountName', 'isRegistered', 'stakeRequirement', 'isFreeosEnabled', 'totalFreeos', 'liquidFreeos', 'liquidOptions', 'canClaim', 'reasonCannotClaim', 'currentIteration', 'nextIteration', 'airkeyBalance', 'airclaimStatus', 'currentPrice', 'targetPrice']),
+    thresholdRangeUpper() {
+      return this.currentPrice < this.thresholdRangeLower ? Math.floor(parseFloat(this.lockFactor) * this.thresholdRangeLower * 10000000) / 10000000 : Math.floor(parseFloat(this.lockFactor) * this.currentPrice * 10000000) / 10000000;
     },
-    lockingThresholdVoteInvalid(){
-        var val = parseFloat(this.lockingThresholdVote);
-        if(typeof val !== 'number' || val < this.thresholdRangeLower || val > this.thresholdRangeUpper){
-          return true;
-        }else{
-          return false;
-        }
+    lockingThresholdVoteInvalid() {
+      var val = parseFloat(this.lockingThresholdVote);
+      if (typeof val !== 'number' || val < this.thresholdRangeLower || val > this.thresholdRangeUpper) {
+        return true;
+      } else {
+        return false;
+      }
     }
   },
   created: function () {
 
   },
   watch: {
-    lockingThresholdVote(newValue, oldValue){
-        var val = parseFloat(newValue);
-        if(typeof val !== 'number' || val < this.thresholdRangeLower || val > this.thresholdRangeUpper){
-          this.lockingThresholdVoteInvalid = true;
-        }else{
-          this.lockingThresholdVoteInvalid = false;
-        }
+    lockingThresholdVote(newValue, oldValue) {
+      var val = parseFloat(newValue);
+      if (typeof val !== 'number' || val < this.thresholdRangeLower || val > this.thresholdRangeUpper) {
+        this.lockingThresholdVoteInvalid = true;
+      } else {
+        this.lockingThresholdVoteInvalid = false;
+      }
     }
   },
   methods: {
-        ...mapActions('freeos', ['vote']),
-        async submit() {
-            if (this.userHasStaked && this.lockingThresholdVote) {
-                const _ = this;
-                var result = await _.vote({user:this.accountName, q3response:this.lockingThresholdVote})
-                if (!(result instanceof Error)) {
-                    this.$refs.complete.openDialog({
-                        title: "Woohoo", subtitle: "Thanks for Voting!", text: "You Voted:", value: this.lockingThresholdVote, currency: "USD"
-                    });
-                }
-            }
-        },
+    ...mapActions('freeos', ['vote']),
+    async submit() {
+      if (this.userHasStaked && this.lockingThresholdVote) {
+        const _ = this;
+        var result = await _.vote({ user: this.accountName, q3response: this.lockingThresholdVote })
+        if (!(result instanceof Error)) {
+          this.$refs.complete.openDialog({
+            title: "Woohoo", subtitle: "Thanks for Voting!", text: "You Voted:", value: this.lockingThresholdVote, currency: "USD"
+          });
+        }
+      }
+    },
   }
 }
 </script>
 
 <style>
-
-
-.q-slider__marker-labels-container .q-slider__marker-labels{
-  display: none; 
-  font-size:12px;
+.q-slider__marker-labels-container .q-slider__marker-labels {
+  display: none;
+  font-size: 12px;
 }
-.q-slider__marker-labels-container .q-slider__marker-labels:first-child,.q-slider__marker-labels-container .q-slider__marker-labels:last-child{
+
+.q-slider__marker-labels-container .q-slider__marker-labels:first-child,
+.q-slider__marker-labels-container .q-slider__marker-labels:last-child {
   display: block;
   line-height: 1;
 }
-
 </style>
