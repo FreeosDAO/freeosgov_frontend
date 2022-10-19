b2b64b0d5b42e8641375998d228f1a8c0338a776
Stuart McPherson <stuart.mcpherson@flipmind.com>
2022-10-19 15:33:29 +1300

    v0.3.6 Beta env vars

--- envparser.js
+++ envparser.js
@@ -2,9 +2,9 @@ const DotEnv = require('dotenv')
 const parsedEnv = DotEnv.config().parsed
 module.exports = function (ctx) {
 
-  parsedEnv['FREEOSGOV_CONTRACT'] = ctx.dev ? "freeosgov2" : "freeosgov2"
-  parsedEnv['FREEOSTOKENS_CONTRACT'] = ctx.dev ? "freeostokens" : "freeostokens"
-  parsedEnv['FREEBITOKENS_CONTRACT'] = ctx.dev ? "freebi" : "freebi"
+  parsedEnv['FREEOSGOV_CONTRACT'] = ctx.dev ? "betaclaim" : "betaclaim"
+  parsedEnv['FREEOSTOKENS_CONTRACT'] = ctx.dev ? "betabeta" : "betabeta"
+  parsedEnv['FREEBITOKENS_CONTRACT'] = ctx.dev ? "betabeebi" : "betabeebi"
   parsedEnv['TIMED_FETCH_DELAY'] = ctx.dev ? 5000 : 5000
   parsedEnv['VUE_ROUTER_MODE'] = ctx.dev ? "hash" : "history"
 
